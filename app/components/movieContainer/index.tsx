92c46a663f3af60a18ab6b2f7494b0c74aec3e82
iamkaranpatel <krn.rj.pte@gmail.com>
2024-04-12 01:07:06 +0530

    movie list by year api added

--- app/components/movieContainer/index.tsx
+++ app/components/movieContainer/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Filter from "../filter";
-import { getAllMovieGenre } from "@/app/utils/endpoint";
-import { MovieGenre } from "@/app/utils/types";
+import { getAllMovieByYear, getAllMovieGenre } from "@/app/utils/endpoint";
+import { MovieDetails, MovieGenre } from "@/app/utils/types";
+import Image from "next/image";
 
 const MovieContainer = () => {
   const [movieGenres, setMovieGenres] = useState<MovieGenre[]>([]);
+  const [movieList, setMovieList] = useState<[]>([]);
 
   useEffect(() => {
     const fetchGenere = async () => {
@@ -23,8 +25,40 @@ const MovieContainer = () => {
     fetchGenere();
   }, []);
 
+  useEffect(() => {
+    const fetchMovieByYear = async () => {
+      try {
+        const movieList = await getAllMovieByYear(2012);
+        console.log(movieList.results)
+        // if (movieGenres?.genres?.length > 0) {
+        //   setMovieGenres([{ id: 0, name: "All" }, ...movieGenres?.genres]);
+        // }
+        setMovieList(movieList?.results)
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchMovieByYear();
+  }, []);
+
   return <div>
     <Filter movieGenres={movieGenres} />
+    <ul>
+    {
+      !!movieList && movieList.map((details: MovieDetails) => {
+        return <li key={details?.id}>
+          <Image 
+            src={`${process.env.NEXT_PUBLIC_MOVIE_IMAGE_URL}${details?.poster_path}`}
+            width={300}
+            height={400}
+            alt={details?.title}
+          />
+          <h2>{details.title}</h2>
+        </li>
+      })
+    }
+    </ul>
     </div>;
 };
 
--- app/utils/types/index.ts
+++ app/utils/types/index.ts
@@ -2,3 +2,15 @@ export interface MovieGenre {
   id: number;
   name: string;
 }
+
+export interface MovieDetails {
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  backdrop_path: string;
+  poster_path: string;
+  genre_ids: number[]
+  release_date: string
+  vote_average: number
+}
